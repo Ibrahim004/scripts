48b3c82542f71c46d1f597c85df5e89b2453d532
Ibrahim <ibra437@gmail.com>
2024-05-03 11:49:52 -0700

    Implements insertion into BST, updates definition of BST node to use int keys

--- c/trees/BST/BST.c
+++ c/trees/BST/BST.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 
 typedef struct BST {
+    int key;
     void* value;
     struct BST* left;
     struct BST* right;
 } BST;
 
-BST* create_BST_node(void* value){
+BST* create_BST_node(int key, void* value){
     BST* node = (BST*) malloc(sizeof(BST));
     if(node == NULL)
     {
         perror("Could not allocate node");
         exit(EXIT_FAILURE);
     }
+    node->key = key;
     node->value = value;
     node->left = NULL;
     node->right = NULL;
@@ -37,18 +40,67 @@ void free_BST_node(BST* root, void (*deleteValue)(void*))
     free(root);
 }
 
-void insert(void* value)
+BST* insert(BST* root, int key, void* value)
+{
+    if(root == NULL)
+    {
+        return create_BST_node(key, value);
+    }
+    if(key <= root->key)
+    {
+        root->left = insert(root->left, key, value);
+    }
+    else
+    {
+        root->right = insert(root->right, key, value);
+    }
+    return root;
+}
+
+void delete(BST* root, int key)
 {
     // todo
 }
 
-void delete(void* value)
+BST* search(BST* root, int key)
 {
-    // todo
+    if(root == NULL || root->key == key)
+    {
+        return root;
+    }
+    else if(key <= root->key)
+    {
+        return search(root->left, key);
+    }
+    return search(root->right, key);
 }
 
-BST* search(BST* root, void* value)
+void inOrderTraversal(BST* root)
 {
-    // todo
-    return NULL;
+    if(root != NULL)
+    {
+        inOrderTraversal(root->left);
+        printf("%d ", root->key);
+        inOrderTraversal(root->right);
+    }
+}
+
+int main()
+{
+    BST* root = NULL;
+    root = insert(root, 8, NULL);
+    insert(root, 1, NULL);
+    insert(root, 10, NULL);
+    insert(root, 8, NULL);
+    insert(root, 5, NULL);
+    insert(root, 11, NULL);
+    insert(root, 7, NULL);
+    inOrderTraversal(root);
+    printf("\n");
+
+    BST* result = search(root, 1);
+    printf("Got result: %d\n", result->key);
+
+    free_BST_node(root, free);
+    return 0;
 }
